e30d97c95961ed73be08a8460f5e5ca6abe05812
mohammedsalah7 <mohamad_m@windowslive.com>
2020-09-29 14:58:12 +0300

    edit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
 // Here you can access the colors variable
 // have fun
-// function sss(){
-//     const randomColor = colors[Math.floor(Math.random()*colors.length)];
-
-//     document.getElementById("pallete").style.color = randomColor;
-//     console.log(randomColor)
-
-// }
-
-
-
 document.getElementById("btn").addEventListener("click", function(){
     const randomColor = colors[Math.floor(Math.random()*colors.length)];
     const textColor = colors[Math.floor(Math.random()*colors.length)];
